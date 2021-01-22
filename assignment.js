e96debd33d5aa8f3dfd71238cf1f388e8dcebf0e
daud382319 <daudbdfci@gmail.com>
2021-01-22 15:08:04 +0600

    second commit

--- assignment.js
+++ assignment.js
@@ -17,6 +17,37 @@ function bugetCalculator(num1,num2,num3){
 }
 var buget =bugetCalculator(5,2,3);
 console.log(buget);
+//hotelCost
+function hotelCost(day){
+    if(day<=10){
+        var cost=day*100;
+        return cost;
+    }
+    else if(day<=20){
+        var cost =day*80;
+        return cost;
+
+    }
+    else {
+        var cost=day*50;
+        return cost;
+    }
+}
+var finalCost=hotelCost(30
+    );
+console.log(finalCost);
+// megaFriend
+function megaFriend(arr) {
+    var len = arr.length, max = '';
+    while (len--) {
+      if (arr[len] > max) {
+        max = arr[len];
+      }
+    }
+    return max;
+  };
+var largestName=megaFriend(['Muhammad Daud','Johir','Monir']);
+console.log(largestName);
 
 
 
