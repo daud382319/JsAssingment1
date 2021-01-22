a9ce940ddefd79bee1d1984547957462ad508e57
daud382319 <daudbdfci@gmail.com>
2021-01-22 12:32:55 +0600

    first commit

---
+++ assignment.js
@@ -0,0 +1,101 @@
+function kilometerToMeter(kilometer)
+{
+    var meter = kilometer * 1000;
+    return meter;
+}
+var outPut = kilometerToMeter(5);
+console.log(outPut);
+
+
+//bugetCalculator
+function bugetCalculator(num1,num2,num3){
+    var watch =num1*50;
+    var phone=num2*100;
+    var leptop=num3*500;
+    var total=watch+phone+leptop;
+    return total;
+}
+var buget =bugetCalculator(5,2,3);
+console.log(buget);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
